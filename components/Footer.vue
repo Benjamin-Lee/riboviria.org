32447a1e5ff6b76d6adb28ac6fa877f487031444
Benjamin Lee <benjamindlee@me.com>
2022-02-14 20:08:37 -0500

    Make the navbar footer link point to the GitHub

--- components/Footer.vue
+++ components/Footer.vue
@@ -5,7 +5,11 @@
       class="max-w-7xl mx-auto py-12 px-4 sm:px-6 md:flex md:items-center md:justify-between lg:px-8"
     >
       <div class="flex justify-center space-x-6 md:order-2">
-        <a href="#" class="text-gray-400 hover:text-gray-500">
+        <a
+          href="https://github.com/Benjamin-Lee/riboviria.org"
+          target="_blank"
+          class="text-gray-400 hover:text-gray-500"
+        >
           <span class="sr-only">GitHub</span>
           <svg
             class="h-6 w-6"
