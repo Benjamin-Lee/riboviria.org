b6754878ec9010a2a8d6ad9f32c4c3689c790fa1
Benjamin Lee <benjamindlee@me.com>
2022-02-17 10:58:29 -0700

    Add the github link in the footer

--- components/Footer.vue
+++ components/Footer.vue
@@ -5,11 +5,7 @@
       class="max-w-7xl mx-auto py-12 px-4 sm:px-6 md:flex md:items-center md:justify-between lg:px-8"
     >
       <div class="flex justify-center space-x-6 md:order-2">
-        <a
-          href="https://github.com/Benjamin-Lee/riboviria.org"
-          target="_blank"
-          class="text-gray-400 hover:text-gray-500"
-        >
+        <a href="https://github.com/Benjamin-Lee/riboviria.org" target="_blank" class="text-gray-400 hover:text-gray-500">
           <span class="sr-only">GitHub</span>
           <svg
             class="h-6 w-6"
