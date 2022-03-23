666aca5ccfe9fa686086223e1677ae6fa8f87b1b
Benjamin Lee <benjamindlee@me.com>
2022-03-23 13:08:22 -0400

    Reword the hero

--- components/Hero.vue
+++ components/Hero.vue
@@ -10,8 +10,10 @@
             <h1
               class="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl lg:text-5xl xl:text-6xl"
             >
-              <span class="block xl:inline">Uncover the global</span>
-              <span class="block text-green-500 xl:inline">RNA virome</span>
+              <span class="block"
+                >The RNA Viruses in Metatransciptomes Discovery Project"</span
+              >
+              <!-- <span class="block text-green-500 xl:inline">RNA virome</span> -->
             </h1>
             <p
               class="mt-3 max-w-md mx-auto text-lg text-gray-500 sm:text-xl md:mt-5 md:max-w-3xl"
